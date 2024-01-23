6f9902dc965fd88ddc5b01367ff29ace7223d00f
oryxoik <oryxoik@gmail.com>
2024-01-23 10:36:38 +0500

    Update CustomLogicInputBuiltin.cs

--- Assets/Scripts/CustomLogic/Builtin/CustomLogicInputBuiltin.cs
+++ Assets/Scripts/CustomLogic/Builtin/CustomLogicInputBuiltin.cs
@@ -1,4 +1,5 @@
-﻿using ApplicationManagers;
+﻿using System;
+using ApplicationManagers;
 using Characters;
 using Settings;
 using System.Collections.Generic;
@@ -15,17 +16,38 @@ namespace CustomLogic
 
         public override object CallMethod(string name, List<object> parameters)
         {
-            if (name == "GetKeyHold" || name == "GetKeyDown" || name == "GetKeyUp")
+            var keyHold = name == "GetKeyHold";
+            var keyDown = name == "GetKeyDown";
+            var keyUp = name == "GetKeyUp";
+            
+            if (keyHold || keyDown || keyUp)
             {
                 string key = (string)parameters[0];
-                string[] strArr = key.Split('/');
-                KeybindSetting keybind = (KeybindSetting)((SaveableSettingsContainer)SettingsManager.InputSettings.Settings[strArr[0]]).Settings[strArr[1]];
-                if (name == "GetKeyDown")
-                    return keybind.GetKeyDown();
-                if (name == "GetKeyUp")
-                    return keybind.GetKeyUp();
-                if (name == "GetKeyHold")
-                    return keybind.GetKey();
+
+                if (key.Contains('/'))
+                {
+                    string[] strArr = key.Split('/');
+                    KeybindSetting keybind =
+                        (KeybindSetting)((SaveableSettingsContainer)SettingsManager.InputSettings.Settings[strArr[0]])
+                        .Settings[strArr[1]];
+
+                    if (keyDown)
+                        return keybind.GetKeyDown();
+                    if (keyUp)
+                        return keybind.GetKeyUp();
+                    if (keyHold)
+                        return keybind.GetKey();
+                }
+                else if (Enum.TryParse(key, true, out KeyCode keyCode))
+                {
+                    if (keyDown)
+                        return Input.GetKeyDown(keyCode);
+                    if (keyUp)
+                        return Input.GetKeyUp(keyCode);
+                    if (keyHold)
+                        return Input.GetKey(keyCode);
+                }
+
                 return null;
             }
             if (name == "GetKeyName")
